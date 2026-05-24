b5b0a209e42d4362cfbab87ac5d3ac44fa161594
Sararellano <sara.arellano@selectra.info>
2026-05-24 02:42:22 +0200

    Updates

--- precache-manifest.a1a11d2dba30213a08a5849ceed2c3f2.js
+++ precache-manifest.a1a11d2dba30213a08a5849ceed2c3f2.js
@@ -1,35 +1,39 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b109ed52c6d9aca5e6400ce5afb745a",
+    "revision": "fef33f4e6c458c8d01bba9f15f4501f9",
     "url": "/sararellano/index.html"
   },
   {
-    "revision": "7456c48378e225079b46",
-    "url": "/sararellano/static/css/main.a998d389.chunk.css"
+    "revision": "9444dbe15a4ce8359371",
+    "url": "/sararellano/static/css/main.04b22fcd.chunk.css"
   },
   {
-    "revision": "4e88dabd8f963d3c97ac",
-    "url": "/sararellano/static/js/2.d1df6579.chunk.js"
+    "revision": "ab180a860cbda9f0a417",
+    "url": "/sararellano/static/js/2.5790eb7f.chunk.js"
   },
   {
-    "revision": "7456c48378e225079b46",
-    "url": "/sararellano/static/js/main.493850f8.chunk.js"
+    "revision": "9444dbe15a4ce8359371",
+    "url": "/sararellano/static/js/main.66e603fb.chunk.js"
   },
   {
     "revision": "f2855cc37f861a04e26c",
     "url": "/sararellano/static/js/runtime~main.e0f338f5.js"
   },
   {
-    "revision": "60057a9be74e73cf5c256c138a6d436c",
-    "url": "/sararellano/static/media/CV-SaraArellano-en.60057a9b.pdf"
+    "revision": "c0c64324c6ab426b17bcc4e87bdaf149",
+    "url": "/sararellano/static/media/CV-SaraArellano-EN.c0c64324.pdf"
   },
   {
-    "revision": "4a00d89f223a54310f3ccdf0adbbda92",
-    "url": "/sararellano/static/media/certificadoUdemy.4a00d89f.jpeg"
+    "revision": "ee8e241adeeab48b9d671de959f0b06d",
+    "url": "/sararellano/static/media/CV-SaraArellano-ES.ee8e241a.pdf"
   },
   {
     "revision": "91221aeba452aeb57c55826c70920835",
     "url": "/sararellano/static/media/es.91221aeb.svg"
+  },
+  {
+    "revision": "b58e9f024a7df1a4d2a6e44afca2e57e",
+    "url": "/sararellano/static/media/fotoSaraArellano.b58e9f02.webp"
   },
   {
     "revision": "fcfef6780b36bef537381474df9d0be9",
@@ -40,19 +44,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sararellano/static/media/gear-audiovisuals.81d71834.png"
   },
   {
-    "revision": "75065b033be0819d223ff931f7116895",
-    "url": "/sararellano/static/media/inicio.75065b03.jpg"
-  },
-  {
     "revision": "6a89454b0b7d182baf43405ff4c560dd",
     "url": "/sararellano/static/media/jsfisio.6a89454b.jpg"
   },
   {
     "revision": "4294b68e4b94746874a9c898d6adc3c2",
     "url": "/sararellano/static/media/navalpie.4294b68e.png"
-  },
-  {
-    "revision": "0c3056219ea76fcef6fb323e264c0a4e",
-    "url": "/sararellano/static/media/sararellano.0c305621.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sararellano/precache-manifest.889492927ec68894862be9e8ccd658d9.js"
+  "/sararellano/precache-manifest.a1a11d2dba30213a08a5849ceed2c3f2.js"
 );
 
 self.addEventListener('message', (event) => {
